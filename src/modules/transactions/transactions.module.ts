2b55c556bb8aede3951a29b756aced301a30090b
Tolu Afolayan <31620246+tolukusan@users.noreply.github.com>
2026-02-16 10:16:28 +0100

    modify transaction module

--- src/modules/transactions/transactions.module.ts
+++ src/modules/transactions/transactions.module.ts
@@ -4,9 +4,10 @@ import { HttpModule } from '@nestjs/axios';
 import { TransactionsService } from './transactions.service';
 import { FlutterwaveService } from './flutterwave.service';
 import { LedgerModule } from '../ledger/ledger.module';
+import { PrismaModule } from '@/prisma';
 
 @Module({
-  imports: [HttpModule, LedgerModule],
+  imports: [HttpModule, LedgerModule, PrismaModule],
   providers: [TransactionsService, FlutterwaveService],
   exports: [TransactionsService, FlutterwaveService],
 })
